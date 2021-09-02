b7862147e50e6889ea36bdf0c1022b15fef7c3ab
josephkem <josephkemoy@gmail.com>
2021-09-01 21:28:19 -0400

    Created template for homepage and added sass styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <div className="header">Hello World</div>
+      <Home />
     </div>
   );
 }
