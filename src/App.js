4b84ef05060a404b2dca67c9b0197fc44b29130a
josephkem <josephkemoy@gmail.com>
2021-09-02 20:50:53 -0400

    Added component to render shop page and items on it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
 import Home from "./pages/Home/Home";
+import Shop from "./pages/Shop/Shop";
 import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/shop" component={Shop} />
+      </Switch>
     </div>
   );
 }
