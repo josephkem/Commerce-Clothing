25add4e91323d8a08c7b611b488e7d5bfc9b54b8
josephkem <josephkemoy@gmail.com>
2021-09-12 08:01:08 -0400

    Added a header comoponent along with some styling and a logo

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import Shop from "./pages/Shop/Shop";
 import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/shop" component={Shop} />
