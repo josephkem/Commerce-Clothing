0fd938ad82fe79c028208dc28a65888e207762d0
josephkem <josephkemoy@gmail.com>
2021-09-26 15:44:08 -0400

    Modified main app to get user from firebase and header component to display sign in and sign out buttons based on user

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import React from "react";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Shop from "./pages/Shop/Shop";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/header/Header";
 import Auth from "./pages/Auth/Auth";
+import { auth } from "./firebase/firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/shop" component={Shop} />
-        <Route exact path="/signin" component={Auth} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/signin" component={Auth} />
+        </Switch>
+      </div>
+    );
+  }
+  r;
 }
 
 export default App;
