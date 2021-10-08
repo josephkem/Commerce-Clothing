56afea1e6f4d7a5ad69fc73fe0bb0664fee4581a
josephkem <josephkemoy@gmail.com>
2021-10-07 20:56:26 -0400

    updated the cart reducer so that it adds cart items and hooked it up to the collection items component

--- src/components/collection-item/CollectionItem.js
+++ src/components/collection-item/CollectionItem.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addItem } from "../../redux/cart/cartActions";
 import CustomButton from "../custom-button/CustomButton";
 import "./collection_item.styles.scss";
 
-export const CollectionItem = ({ id, name, price, imageUrl }) => {
+export const CollectionItem = ({ item, addItem }) => {
+  const { name, price, imageUrl } = item;
   return (
     <div className="collection-item">
       <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
@@ -11,9 +14,15 @@ export const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{price}</span>
       </div>
-      <CustomButton inverted>Add to cart</CustomButton>
+      <CustomButton onClick={() => addItem(item)} inverted>
+        Add to cart
+      </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
