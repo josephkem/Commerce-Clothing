b54e85a24c8c5769cf0f7c3a4994941333cc3c9e
josephkem <josephkemoy@gmail.com>
2021-10-09 19:29:04 -0400

    Updated cart reducer to remove items and hooked up the functions to the arrows in the checkout item component

--- src/components/checkout-item/CheckoutItem.js
+++ src/components/checkout-item/CheckoutItem.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
-import { clearItemFromCart } from "../../redux/cart/cartActions";
+import { removeItemFromCart } from "../../redux/cart/cart.utils";
+import {
+  addItem,
+  clearItemFromCart,
+  removeItem,
+} from "../../redux/cart/cartActions";
+import CartItem from "../cart-item/CartItem";
 import "./checkout-item.styles.scss";
 
-function CheckoutItem({ cartItem, clearItem }) {
+function CheckoutItem({ cartItem, clearItem, addItem, removeItem }) {
   const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
@@ -12,9 +18,13 @@ function CheckoutItem({ cartItem, clearItem }) {
       </div>
       <span className="name">{name}</span>
       <span className="quantity">
-        <div className="arrow">&#10094;</div>
+        <div className="arrow" onClick={() => removeItem(cartItem)}>
+          &#10094;
+        </div>
         <span className="value">{quantity}</span>
-        <div className="arrow">&#10095;</div>
+        <div className="arrow" onClick={() => addItem(cartItem)}>
+          &#10095;
+        </div>
       </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => clearItem(cartItem)}>
@@ -26,6 +36,8 @@ function CheckoutItem({ cartItem, clearItem }) {
 
 const mapDispatchToProps = (dispatch) => ({
   clearItem: (item) => dispatch(clearItemFromCart(item)),
+  addItem: (item) => dispatch(addItem(item)),
+  removeItem: (item) => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
