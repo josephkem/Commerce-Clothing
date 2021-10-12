32f2a0fd18736515513d7d75caa982ab2a7479d6
josephkem <josephkemoy@gmail.com>
2021-10-12 13:29:23 -0400

    Updated redux and created selectors to add the shop directory to the state of the app

--- src/redux/RootReducer.js
+++ src/redux/RootReducer.js
@@ -3,6 +3,7 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
+import directoryReducer from "./directory/directoryReducer";
 
 const persistConfig = {
   key: "root",
@@ -13,6 +14,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
 });
 
 export default persistReducer(persistConfig, rootReducer);
